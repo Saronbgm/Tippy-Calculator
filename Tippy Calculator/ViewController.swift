b1a7843d50a821ccbb509b3df229733e3cd5e888
Saron G <shariberhane@gmail.com>
2020-08-13 12:30:33 -0400

    Initial Commit

---
+++ Tippy Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy Calculator
+//
+//  Created by Saron Berhane Ghebremariam on 8/13/20.
+//  Copyright © 2020 Saron Ghebremariam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
